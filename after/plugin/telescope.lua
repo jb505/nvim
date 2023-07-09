04d041cba5e266c374af985302780245ac16dc1a
James Beauford <jbad505@gmail.com>
2023-07-08 20:07:58 -0600

    Comments for telescope.lua.

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,6 +1,9 @@
 local builtin = require('telescope.builtin')
+-- Fuzzy find
 vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+-- Git files
 vim.keymap.set('n', '<leader>gf', builtin.git_files, {})
+-- File search
 vim.keymap.set('n', '<leader>fs', function()
 	builtin.grep_string({ search = vim.fn.input("Grep > ") });
 end)
