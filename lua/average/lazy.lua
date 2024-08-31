e4d13688776d3343ad0f497ec50ab6abe31f2605
jbad505 <jbad505@gmail.com>
2024-08-30 18:12:50 -0600

    Modified lazy.lua & set.lua

--- lua/average/lazy.lua
+++ lua/average/lazy.lua
@@ -216,6 +216,9 @@ require('lazy').setup({
     event = "InsertEnter",
     opts = {} -- this is equalent to setup({}) function
   },
+  {
+    'ThePrimeagen/vim-be-good' -- Some wirdo made this game...
+  },
   -- NOTE: Next Step on Your Neovim Journey: Add/Configure additional "plugins" for kickstart
   --       These are some example plugins that I've included in the kickstart repository.
   --       Uncomment any of the lines below to enable them.
